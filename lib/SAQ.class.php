2ca429a03c6dd6a57e593f83ed6b42d7e128b34c
haithem-Ayeb <52929720+haithem-Ayeb@users.noreply.github.com>
2020-09-11 12:26:41 +0100

    nettoyer le lien de la photo récupérée sur le site de la SAQ

--- lib/SAQ.class.php
+++ lib/SAQ.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Class MonSQL
  * Classe qui génère ma connection à MySQL à travers un singleton
@@ -10,7 +11,8 @@
  *
  *
  */
-class SAQ extends Modele {
+class SAQ extends Modele
+{
 
 	const DUPLICATION = 'duplication';
 	const ERREURDB = 'erreurdb';
@@ -20,11 +22,12 @@ class SAQ extends Modele {
 	private static $_status;
 	private $stmt;
 
-	public function __construct() {
+	public function __construct()
+	{
 		parent::__construct();
-		if (!($this -> stmt = $this -> _db -> prepare("INSERT INTO vino__bouteille(nom, id_type, image, code_saq, pays, description, prix_saq, url_saq, url_img, format) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"))) {
+		if (!($this->stmt = $this->_db->prepare("INSERT INTO vino__bouteille(nom, id_type, image, code_saq, pays, description, prix_saq, url_saq, url_img, format) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"))) {
 			//var_dump($this -> stmt);
-			echo "Echec de la préparation : (" . $mysqli -> errno . ") " . $mysqli -> error;
+			echo "Echec de la préparation : (" . $mysqli->errno . ") " . $mysqli->error;
 		}
 	}
 
@@ -33,7 +36,8 @@ class SAQ extends Modele {
 	 * @param int $nombre
 	 * @param int $debut
 	 */
-	public function getProduits($nombre = 24, $page = 1) {
+	public function getProduits($nombre = 24, $page = 1)
+	{
 		$s = curl_init();
 		$url = "https://www.saq.com/fr/produits/vin/vin-rouge?p=1&product_list_limit=24&product_list_order=name_asc";
 		//curl_setopt($s, CURLOPT_URL, "http://www.saq.com/webapp/wcs/stores/servlet/SearchDisplay?searchType=&orderBy=&categoryIdentifier=06&showOnly=product&langId=-2&beginIndex=".$debut."&tri=&metaData=YWRpX2YxOjA8TVRAU1A%2BYWRpX2Y5OjE%3D&pageSize=". $nombre ."&catalogId=50000&searchTerm=*&sensTri=&pageView=&facet=&categoryId=39919&storeId=20002");
@@ -47,23 +51,23 @@ class SAQ extends Modele {
 		curl_close($s);
 
 		$doc = new DOMDocument();
-		$doc -> recover = true;
-		$doc -> strictErrorChecking = false;
-		@$doc -> loadHTML(self::$_webpage);
-		$elements = $doc -> getElementsByTagName("li");
+		$doc->recover = true;
+		$doc->strictErrorChecking = false;
+		@$doc->loadHTML(self::$_webpage);
+		$elements = $doc->getElementsByTagName("li");
 		$i = 0;
-		
+
 		foreach ($elements as $key => $noeud) {
 			//var_dump($noeud -> getAttribute('class')) ;
 			//if ("resultats_product" == str$noeud -> getAttribute('class')) {
-			if (strpos($noeud -> getAttribute('class'), "product-item") !== false) {
+			if (strpos($noeud->getAttribute('class'), "product-item") !== false) {
 
 				//echo $this->get_inner_html($noeud);
 				$info = self::recupereInfo($noeud);
-				echo "<p>".$info->nom;
-				$retour = $this -> ajouteProduit($info);
-				echo "<br>Code de retour : " . $retour -> raison . "<br>";
-				if ($retour -> succes == false) {
+				echo "<p>" . $info->nom;
+				$retour = $this->ajouteProduit($info);
+				echo "<br>Code de retour : " . $retour->raison . "<br>";
+				if ($retour->succes == false) {
 					echo "<pre>";
 					//var_dump($info);
 					echo "</pre>";
@@ -78,109 +82,110 @@ class SAQ extends Modele {
 		return $i;
 	}
 
-	private function get_inner_html($node) {
+	private function get_inner_html($node)
+	{
 		$innerHTML = '';
-		$children = $node -> childNodes;
+		$children = $node->childNodes;
 		foreach ($children as $child) {
-			$innerHTML .= $child -> ownerDocument -> saveXML($child);
+			$innerHTML .= $child->ownerDocument->saveXML($child);
 		}
 
 		return $innerHTML;
 	}
 	private function nettoyerEspace($chaine)
 	{
-		return preg_replace('/\s+/', ' ',$chaine);
+		return preg_replace('/\s+/', ' ', $chaine);
 	}
-	private function recupereInfo($noeud) {
-		
+	private function recupereInfo($noeud)
+	{
+
 		$info = new stdClass();
-		$info -> img = $noeud -> getElementsByTagName("img") -> item(0) -> getAttribute('src'); //TODO : Nettoyer le lien
-		;
-		$a_titre = $noeud -> getElementsByTagName("a") -> item(0);
-		$info -> url = $a_titre->getAttribute('href');
-		
-		$info -> nom = self::nettoyerEspace(trim($a_titre -> textContent));	//TODO : Retirer le format de la bouteille du titre.
-		
+		$info->img = $noeud->getElementsByTagName("img")->item(0)->getAttribute('src'); //TODO : Nettoyer le lien
+		$info->img = substr($info->img, 6); // Enlever le https au début du lien
+		if (preg_match("/\png$\b/i", $info->img)) { // standarsiser la taille de la photo récupérée
+			$info->img = $info->img . "?quality=80&fit=bounds&height=166&width=111&canvas=111:166";
+		}
+		var_dump($info->img);
+
+		$a_titre = $noeud->getElementsByTagName("a")->item(0);
+		$info->url = $a_titre->getAttribute('href');
+
+		$info->nom = self::nettoyerEspace(trim($a_titre->textContent));	//TODO : Retirer le format de la bouteille du titre.
+		var_dump($info->nom);
+
 		// Type, format et pays
-		$aElements = $noeud -> getElementsByTagName("strong");
+		$aElements = $noeud->getElementsByTagName("strong");
 		foreach ($aElements as $node) {
-			if ($node -> getAttribute('class') == 'product product-item-identity-format') {
-				$info -> desc = new stdClass();
-				$info -> desc -> texte = $node -> textContent;
+			if ($node->getAttribute('class') == 'product product-item-identity-format') {
+				$info->desc = new stdClass();
+				$info->desc->texte = $node->textContent;
 				$info->desc->texte = self::nettoyerEspace($info->desc->texte);
 				$aDesc = explode("|", $info->desc->texte); // Type, Format, Pays
-				if (count ($aDesc) == 3) {
-					
-					$info -> desc -> type = trim($aDesc[0]);
-					$info -> desc -> format = trim($aDesc[1]);
-					$info -> desc -> pays = trim($aDesc[2]);
+				if (count($aDesc) == 3) {
+
+					$info->desc->type = trim($aDesc[0]);
+					$info->desc->format = trim($aDesc[1]);
+					$info->desc->pays = trim($aDesc[2]);
 				}
-				
-				$info -> desc -> texte = trim($info -> desc -> texte);
+
+				$info->desc->texte = trim($info->desc->texte);
 			}
 		}
 
 		//Code SAQ
-		$aElements = $noeud -> getElementsByTagName("div");
+		$aElements = $noeud->getElementsByTagName("div");
 		foreach ($aElements as $node) {
-			if ($node -> getAttribute('class') == 'saq-code') {
-				if(preg_match("/\d+/", $node -> textContent, $aRes))
-				{
-					$info -> desc -> code_SAQ = trim($aRes[0]);
+			if ($node->getAttribute('class') == 'saq-code') {
+				if (preg_match("/\d+/", $node->textContent, $aRes)) {
+					$info->desc->code_SAQ = trim($aRes[0]);
 				}
-				
-				
-				
 			}
 		}
 
-		$aElements = $noeud -> getElementsByTagName("span");
+		$aElements = $noeud->getElementsByTagName("span");
 		foreach ($aElements as $node) {
-			if ($node -> getAttribute('class') == 'price') {
-				$info -> prix = trim($node -> textContent);				
+			if ($node->getAttribute('class') == 'price') {
+				$info->prix = trim($node->textContent);
 			}
 		}
 		//var_dump($info);
 		return $info;
 	}
 
-	private function ajouteProduit($bte) {
+	private function ajouteProduit($bte)
+	{
 		$retour = new stdClass();
-		$retour -> succes = false;
-		$retour -> raison = '';
+		$retour->succes = false;
+		$retour->raison = '';
 
 		//var_dump($bte);
 		// Récupère le type
-		$rows = $this -> _db -> query("select id from vino__bouteille_type where type = '" . $bte -> desc -> type . "'");
-	
-		if ($rows -> num_rows == 1) {
-			$type = $rows -> fetch_assoc();
+		$rows = $this->_db->query("select id from vino__bouteille_type where type = '" . $bte->desc->type . "'");
+
+		if ($rows->num_rows == 1) {
+			$type = $rows->fetch_assoc();
 			//var_dump($type);
 			$type = $type['id'];
 			//var_dump($type);
 
-			$rows = $this -> _db -> query("select id from vino__bouteille where code_saq = '" . $bte -> desc -> code_SAQ . "'");
-			$prix_mod = substr(($bte -> prix), 0,5);
+			$rows = $this->_db->query("select id from vino__bouteille where code_saq = '" . $bte->desc->code_SAQ . "'");
+			$prix_mod = substr(($bte->prix), 0, 5);
 			$prix_mod = str_replace(',', '.', $prix_mod);
-			$prix_mod = (float)$prix_mod;
-			
-			if ($rows -> num_rows < 1) {
-				$this -> stmt -> bind_param("sissssdsss", $bte -> nom, $type, $bte -> img, $bte -> desc -> code_SAQ, $bte -> desc -> pays, $bte -> desc -> texte, $prix_mod, $bte -> url, $bte -> img, $bte -> desc -> format);
-				$retour -> succes = $this -> stmt -> execute();
-				$retour -> raison = self::INSERE;
+			$prix_mod = (float) $prix_mod;
+
+			if ($rows->num_rows < 1) {
+				$this->stmt->bind_param("sissssdsss", $bte->nom, $type, $bte->img, $bte->desc->code_SAQ, $bte->desc->pays, $bte->desc->texte, $prix_mod, $bte->url, $bte->img, $bte->desc->format);
+				$retour->succes = $this->stmt->execute();
+				$retour->raison = self::INSERE;
 				var_dump($this->stmt);
 			} else {
-				$retour -> succes = false;
-				$retour -> raison = self::DUPLICATION;
+				$retour->succes = false;
+				$retour->raison = self::DUPLICATION;
 			}
 		} else {
-			$retour -> succes = false;
-			$retour -> raison = self::ERREURDB;
-
+			$retour->succes = false;
+			$retour->raison = self::ERREURDB;
 		}
 		return $retour;
-
 	}
-
 }
-?>
