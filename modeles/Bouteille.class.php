87f3758645a361b00c75023ccadb54e43a1f3ada
karima harabidi <karima.harabidi.yahoo.fr>
2020-09-15 18:38:26 -0400

    rectifier la requete sql getListeBouteilleCellier()

--- modeles/Bouteille.class.php
+++ modeles/Bouteille.class.php
@@ -29,25 +29,39 @@ class Bouteille extends Modele
 		return $rows;
 	}
 
-	public function getListeBouteilleCellier()
+	public function getListeBouteilleCellier($id_cellier='',$pays=''/*$id, ='',$type ='',$quantite ='',$millesime=''*/)
 	{
 
 		$rows = array();
-		$requete = 'SELECT v_c_b.id_cellier,v_c_b.id_bouteille,v_c_b.date_achat,
+		$requete = 'SELECT 
+		v_c_b.id_cellier,v_c_b.id_bouteille,v_c_b.date_achat,
 		v_c_b.garde_jusqua,v_c_b.notes,v_c_b.prix,v_c_b.quantite,v_c_b.millesime,
-		v_b.nom,v_b.pays,v_b_t.type,v_b.url_saq,v_b.image
+		v_b.nom,v_b_t.type,v_b.url_saq,v_b.image,
+		v_c.nom_cellier,v_c_b.quantite,v_b.pays
 		FROM vino__cellier_bouteille AS v_c_b 
         INNER JOIN vino__cellier v_c ON v_c.id = v_c_b.id_cellier 
         INNER JOIN vino__bouteille v_b ON v_b.id = v_c_b.id_bouteille 
 		INNER JOIN vino__bouteille_type v_b_t ON v_b_t.id = v_b.id_type
-		INNER JOIN vino__utilisateur v_u ON v_u.id = v_c.id_utilisateur';
-		/*
-		 SELECT * FROM vino__cellier_bouteille AS v_c_b 
-        INNER JOIN vino__cellier v_c ON v_c.id = v_c_b.id_cellier 
-        INNER JOIN vino__bouteille v_b ON v_b.id = v_c_b.id_bouteille 
-        INNER JOIN vino__bouteille_type v_b_t ON v_b_t.id = v_b.id_type
-	 */
-
+		INNER JOIN vino__utilisateur v_u ON v_u.id = v_c.id_utilisateur
+		
+		';
+	if(!empty($id_cellier)){ 
+	$requete.=" WHERE v_c_b.id_cellier ='".$id_cellier."'";
+	}
+	if(!empty($type)){ 
+		$requete.=" AND v_b.id_type ='".$type."'";
+	}
+	if(!empty($quantite)){ 
+		
+		$requete.=" AND v_c_b.quantite ='".$quantite."'";
+		
+	}
+	if(!empty($pays)){ 
+		$requete.=" AND v_b.pays ='".$pays."'";
+	}
+	if(!empty($millesime)){ 
+		$requete.=" AND v_b.millesime ='".$millesime."'";
+	}
 		if (($res = $this->_db->query($requete)) ==	 true) {
 			if ($res->num_rows) {
 				while ($row = $res->fetch_assoc()) {
@@ -59,8 +73,21 @@ class Bouteille extends Modele
 			throw new Exception("Erreur de requête sur la base de donnée", 1);
 			//$this->_db->error;
 		}
-
-
+		return $rows;
+	}
+	/**
+	 * Cette méthode récupère la liste des noms des celliers
+	 * de l'utilisateur connecté 
+	 * 
+	 */
+	public function lireCelliers(){
+		$rows = array();
+		$res = $this->_db->query('Select * from vino__cellier');
+		if ($res->num_rows) {
+			while ($row = $res->fetch_assoc()) {
+				$rows[] = $row;
+			}
+		}
 
 		return $rows;
 	}
