865a8577e16b5cbfd52f3927e8e586e05baade0f
Edna Margarita Rodriguez <ednamargaritar@gmail.com>
2020-09-16 11:09:17 -0400

    Ajout event listener button supprimer

--- js/main.js
+++ js/main.js
@@ -243,5 +243,30 @@ window.addEventListener('load', function() {
                     console.error(error);
                 });
         })
-      })
+    })
+
+    document.querySelectorAll("[name='suprimmerButton']").forEach(item => {
+        item.addEventListener('click', event => {
+            var row = event.target.parentElement.parentElement.parentElement; 
+            console.log(row);
+            var param = {
+                "id_cellier": parseInt(row.getElementsByClassName('idCellier')[0].innerHTML) 
+            };
+            let requete = new Request(BaseURL + "index.php?requete=supprimerCellier", { method: 'POST', body: JSON.stringify(param) });
+            fetch(requete)
+                .then(response => {
+                    if (response.status === 200) {
+                        location.reload();
+                        return response.json();
+                    } else {
+                        throw new Error('Erreur');
+                    }
+                })
+                .then(response => {
+                    console.log(response);
+                }).catch(error => {
+                    console.error(error);
+                });
+        })
+    })
 });
