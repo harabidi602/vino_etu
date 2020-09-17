d41fe17f0380c2d3c8d68de55a7938bd5f9998db
Edna Margarita Rodriguez <ednamargaritar@gmail.com>
2020-09-17 18:34:27 -0400

    changement design des inputs et ajout d'une image

--- vues/nouveauUtilisateur.php
+++ vues/nouveauUtilisateur.php
@@ -1,31 +1,32 @@
 <div class="authentification">
     <div class="formA">
-            <div class="formAuthentification" >
-                <div class="titre">
-                    <h1>Créez votre compte</h1>
-                </div>
-                <form id="nutilisateur" action="" method="post">
+        <div class="formAuthentification" >
+            <div class="titre">
+                <img src="img/newUsager.png" alt="Image Authentification">
+                <h1>Créez votre compte</h1>
+            </div>
+            <form id="nutilisateur" action="" method="post">
                 <?php if(!empty($message)):?>
                 <p><?php echo $message;?></p>
                 <?php endif; ?>
-                <p><input name="nom" placeholder="Nom" required> </p>
+                <p><input class="inputForm" name="nom" placeholder="Nom" required> </p>
                 <p><?= isset($erreurs['nom']) ? $erreurs['nom'] : "" ?></p>
-                <p><input name="prenom" placeholder="Prénom" required></p>
+                <p><input class="inputForm" name="prenom" placeholder="Prénom" required></p>
                 <p><?= isset($erreurs['prenom']) ? $erreurs['prenom'] : "" ?></p>
-                <p><input name="identifiant" placeholder="Identifiant" required> </p>
+                <p><input class="inputForm" name="identifiant" placeholder="Identifiant" required> </p>
                 <p><?= isset($erreurs['identifiant']) ? $erreurs['identifiant'] : "" ?></p>
-                <p><input type="password" name="mdp" placeholder="Mot de passe" required></p>
+                <p><input class="inputForm" type="password" name="mdp" placeholder="Mot de passe" required></p>
                 <p><?= isset($erreurs['mdp']) ? $erreurs['mdp'] : "" ?></p>
-                <p><input type="email" name="courriel" placeholder="Email" required> </p>
+                <p><input class="inputForm" type="email" name="courriel" placeholder="Email" required> </p>
                 <p><?= isset($erreurs['courriel']) ? $erreurs['courriel'] : "" ?></p>
-                <p><input name="telephone" placeholder="Téléphone" required></p>
+                <p><input class="inputForm" name="telephone" placeholder="Téléphone" required></p>
                 <p><?= isset($erreurs['telephone']) ? $erreurs['telephone'] : "" ?></p>
-    
-    <div class="contentForm">
-        <button type="submit" class="creationButton" name="creer">Créez</button>
-        <p class="mdp"> Si vous avez déjà un compte <a href="index.php"> Identifiez-vous </a></p>
-    </div>
-    </form>       
-    </div>
+
+                <div class="contentForm">
+                    <button type="submit" class="creationButton" name="creer">Créez</button>
+                    <p class="mdp"> Si vous avez déjà un compte <a href="index.php"> Identifiez-vous </a></p>
+                </div>
+            </form>       
+        </div>
     </div>
 </div>
