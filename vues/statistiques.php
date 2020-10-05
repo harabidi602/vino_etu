02f4100e06ecdc9b742d3e06b00a809f9734802d
karima harabidi <karima.harabidi.yahoo.fr>
2020-10-05 12:22:17 -0400

    separer les stat

--- vues/statistiques.php
+++ vues/statistiques.php
@@ -1,41 +1,4 @@
    <div class="nouveauUtil" vertical layout>
-    <h4>Statistiques </h4> 
-    <table align="center">
-       <caption>Les nombres d'usager et de cellier</caption>
-        <thead>
-            <tr>
-                <th>Nombre d'usager</th>
-                <th>Nombre de cellier</th>
-            </tr>
-        </thead>
-        <tbody>
-           <?php foreach ($nbUsager as $nb): ?>
-             <td data-column="nusager"><?php echo $nb;?></td>
-            <?php endforeach; ?>
-             <?php foreach ($nbCellier as $nb): ?>
-             <td data-column="nusager"><?php echo $nb;?></td>
-            <?php endforeach; ?>      
-        </tbody>
-    </table>    
-
-    <table align="center">
-       <caption>Le nombre de cellier par usager</caption>
-        <thead>
-            <tr>
-                <th>Usager</th>
-                <th>Nombre de cellier</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($celUsager as $cU) : ?>
-                <tr>
-                    <td data-column="usager"><span><?php echo $cU['usager']; ?></span></td>
-                    <td data-column="cellnombre"><span><?php echo $cU['nombre']; ?></span></td>   
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-  
     <table align="center">
        <caption>Le nombre de bouteille par cellier</caption>
         <thead>
@@ -53,8 +16,6 @@
             <?php endforeach; ?>
         </tbody>
     </table>
-    
-       
     <table align="center">
        <caption>Le nombre de bouteille par usager</caption>
         <thead>
